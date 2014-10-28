0246c3f40085a3072eb5293d95d182397eed5367
Gijs Landwehr <github@fam-landwehr.nl>
2014-10-28 16:37:05 -0500

    driving robot

--- src/org/texastorque/texastorque20145/Robot.java
+++ src/org/texastorque/texastorque20145/Robot.java
@@ -1,13 +1,30 @@
 package org.texastorque.texastorque20145;
 
+import org.texastorque.texastorque20145.input.DriverInput;
+import org.texastorque.texastorque20145.input.InputSystem;
+import org.texastorque.texastorque20145.subsystem.Drivebase;
+import org.texastorque.texastorque20145.torquelib.Parameters;
+
 public class Robot extends TorqueIterative {
+    
+    Parameters params;
+    Drivebase drivebase;
+    
+    InputSystem input;
 
     public void robotInit() {
-
+        params = new Parameters();
+        drivebase = new Drivebase();
+        
+        input = new DriverInput();
     }
 
     public void autonomousPeriodic() {
 
+    }
+    
+    public void teleopInit() {
+        drivebase.setInputSystem(input);
     }
 
     public void teleopPeriodic() {
--- src/org/texastorque/texastorque20145/constants/Constants.java
+++ src/org/texastorque/texastorque20145/constants/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     
     //  -----  Drivebase  -----
     public final static Constant leftDrivePort = new Constant("D_LeftPort", 1);
-    public final static Constant rightDrivePort = new Constant("D_RightPort", 2);
+    public final static Constant rightDrivePort = new Constant("D_RightPort", 9);
     public final static Constant shifterPort = new Constant("D_Shifter", 1);
     public final static Constant leftDriveEncoderAPort = new Constant("D_LeftDriveEncoderA", 8);
     public final static Constant leftDriveEncoderBPort = new Constant("D_LeftDriveEncoderB", 9);
