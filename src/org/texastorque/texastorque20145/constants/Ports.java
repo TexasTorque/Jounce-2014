6e63934787deebe8673ba32b2f470e448fd0ca67
Matthew Webb <matthewwebb65@yahoo.com>
2015-03-15 12:44:21 -0500

    updated ports

--- src/org/texastorque/texastorque20145/constants/Ports.java
+++ src/org/texastorque/texastorque20145/constants/Ports.java
@@ -4,40 +4,40 @@ public class Ports {
 
     //Motors
     public static final int LEFT_DRIVE_PORT = 1;
-    public static final int RIGHT_DRIVE_PORT = 2;
-    public static final int FRONT_ROLLER_PORT = 3;
-    public static final int REAR_ROLLER_PORT = 4;
-    public static final int FRONT_ANGLE_PORT = 5;
-    public static final int REAR_ANGLE_PORT = 6;
-    public static final int SHOOTER_A_PORT = 7;
-    public static final int SHOOTER_B_PORT = 8;
+    public static final int RIGHT_DRIVE_PORT = 9;
+    public static final int FRONT_ROLLER_PORT = 5;
+    public static final int REAR_ROLLER_PORT = 2;
+    public static final int FRONT_ANGLE_PORT = 10;
+    public static final int REAR_ANGLE_PORT = 7;
+    public static final int SHOOTER_A_PORT = 6;
+    public static final int SHOOTER_B_PORT = 4;
 
     //Solenoids
-    public static final int SHIFTER_PORT = 1;
-    public static final int CLAPPER_FRONT_PORT = 2;
+    public static final int SHIFTER_PORT = 4;
+    public static final int CLAPPER_FRONT_PORT = 5;
     public static final int CLAPPER_REAR_PORT = 3;
-    public static final int BACK_WALL_PORT = 4;
+    public static final int BACK_WALL_PORT = 1;
 
     //Encoders
-    public static final int LEFT_DRIVE_ENCODER_A = 1;
-    public static final int LEFT_DRIVE_ENCODER_B = 2;
-    public static final int RIGHT_DRIVE_ENCODER_A = 3;
-    public static final int RIGHT_DRIVE_ENCODER_B = 4;
-    public static final int FRONT_ANGLE_ENCODER_A = 5;
-    public static final int FRONT_ANGLE_ENCODER_B = 6;
-    public static final int REAR_ANGLE_ENCODER_A = 7;
-    public static final int REAR_ANGLE_ENCODER_B = 8;
-    public static final int SHOOTER_ENCODER = 9;
+    public static final int LEFT_DRIVE_ENCODER_A = 6;
+    public static final int LEFT_DRIVE_ENCODER_B = 7;
+    public static final int RIGHT_DRIVE_ENCODER_A = 8;
+    public static final int RIGHT_DRIVE_ENCODER_B = 9;
+    public static final int FRONT_ANGLE_ENCODER_A = 3;
+    public static final int FRONT_ANGLE_ENCODER_B = 2;
+    public static final int REAR_ANGLE_ENCODER_A = 10;
+    public static final int REAR_ANGLE_ENCODER_B = 11;
+    public static final int SHOOTER_ENCODER = 5;
 
     //Buttons
-    public static final int FRONT_INTAKE_BUTTON = 10;
-    public static final int REAR_INTAKE_BUTTON = 11;
+    public static final int FRONT_INTAKE_BUTTON = 4;//?
+    public static final int REAR_INTAKE_BUTTON = 3;//?
 
     //Hall Effect
-    public static final int FRONT_INTAKE_HALL_EFFECT = 12;
-    public static final int REAR_INTAKE_HALL_EFFECT = 14;
+    public static final int FRONT_INTAKE_HALL_EFFECT = 14;
+    public static final int REAR_INTAKE_HALL_EFFECT = 13;
 
     //Pressure
-    public static final int PRESSURE_SWITCH = 14;
-    public static final int COMPRESSOR_RELAY = 1;
+    public static final int PRESSURE_SWITCH = 1;
+    public static final int COMPRESSOR_RELAY = 8;
 }
